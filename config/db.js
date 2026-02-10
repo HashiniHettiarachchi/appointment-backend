2ed90e2628409390dba36dfb58c2b3758970a5c6
Hashini Udeshika <167964475+HashiniHettiarachchi@users.noreply.github.com>
2026-02-10 11:31:51 +0530

    Update MongoDB connection error handling
    Change MongoDB connection error handling to throw error instead of exiting the process.

--- config/db.js
+++ config/db.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
 
+let isConnected = false; // track connection
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log('✅ MongoDB already connected');
+    return;
+  }
+
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
+    isConnected = true;
     console.log('✅ MongoDB Connected Successfully!');
   } catch (error) {
     console.error('❌ MongoDB Connection Failed:', error.message);
-    process.exit(1); // Exit process with failure
+    throw error; // let Vercel handle the error
   }
 };
 
 module.exports = connectDB;
+
